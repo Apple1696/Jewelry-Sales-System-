4d15d7097a6ebbfdcbf55ab2752534b2869c0b6a
HuyPP94 <huyppse184013@fpt.edu.vn>
2024-06-11 09:28:52 +0700

    Add logout function

--- Front-End/src/components/AllRoutes/AllRoutes.jsx
+++ Front-End/src/components/AllRoutes/AllRoutes.jsx
@@ -5,13 +5,10 @@ import Dashboard from './../pages/Dashboard';
 import OrderReport from './../pages/OrderReport';
 import Promotion from './../pages/Promotion';
 import Product from './../pages/Product';
-import Logout from './../pages/Logout';
 import AddProduct from '../pages/AddProduct';
 import Rebuy from './../pages/Rebuy';
 import Sell from './../pages/Sell/Sell';
 import PickPromotion from '../pages/Sell/PickPromotion';
-
-
 
 const AllRoutes = () => {
   return (
@@ -20,18 +17,17 @@ const AllRoutes = () => {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/sell" element={<Sell/>} />
-          <Route path="/pick-promotion" element={<PickPromotion/>} />
+          <Route path="/sell" element={<Sell />} />
+          <Route path="/pick-promotion" element={<PickPromotion />} />
           <Route path="/order-report" element={<OrderReport />} />
           <Route path="/promotion" element={<Promotion />} />
           <Route path="/product" element={<Product />} />
           <Route path="/add-product" element={<AddProduct />} />
           <Route path="/rebuy" element={<Rebuy />} />
-          <Route path="/logout" element={<Logout />} />
         </Routes>
       </Sidebar>
     </BrowserRouter>
-    );
-  };
+  );
+};
 
 export default AllRoutes;
