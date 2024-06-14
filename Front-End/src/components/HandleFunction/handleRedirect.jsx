e856886c37f7eca985b9dacf21f8a446e76aabe6
Ngo Kien <ngochingokien@gmail.com>
2024-06-14 11:14:08 +0700

    Fix css

--- Front-End/src/components/HandleFunction/handleRedirect.jsx
+++ Front-End/src/components/HandleFunction/handleRedirect.jsx
@@ -4,11 +4,9 @@ import { useNavigate } from 'react-router-dom'
 export default function handleRedirect() {
     const navigate = useNavigate();
     const addProduct=()=> navigate('/add-product')
-    // const addPromotion=()=> navigate('/add-promotion')
     const pickPromotion=()=>navigate('/pick-promotion')
     return{
         addProduct,
-        // addPromotion,
         pickPromotion
     }
  
