a867b9f1ba5b51dce31140ad56ac1c495c9f196b
HuyPP94 <156275620+HuyPP94@users.noreply.github.com>
2024-07-12 10:55:08 +0700

    Update EditGem.php

--- app/Filament/Resources/GemResource/Pages/EditGem.php
+++ app/Filament/Resources/GemResource/Pages/EditGem.php
@@ -13,7 +13,7 @@ class EditGem extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()->confirm('Are you sure you want to delete this gem?'),
         ];
     }
 }
